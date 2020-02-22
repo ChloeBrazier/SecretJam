656e566e103cdb20e50bbecc02e0c698544f2e75
Eric Delmonico <ead1758@rit.edu>
2020-02-21 20:05:12 -0500

    Added player controller and camera and merged some strange stuff??

---
+++ Ghost Ship/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 velocity = player.transform.position - transform.position;
+        transform.position += (Vector3)velocity / 20;
+    }
+}
