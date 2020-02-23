ddf3349ae35752d543334673f7509c4efd2899a0
Sean Foley <seantfoley@optonline.net>
2020-02-23 00:01:30 -0500

    FINISHED
    ...or are we

--- Ghost Ship/Assets/Scripts/EndGame.cs
+++ Ghost Ship/Assets/Scripts/EndGame.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets._2D;
 
 public class EndGame : MonoBehaviour
 {
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -19,8 +21,19 @@ public class EndGame : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        player.GetComponent<PlatformerCharacter2D>().Move(0f, false, false);
+        player.GetComponent<PlatformerCharacter2D>().enabled = false;
+        player.GetComponent<Platformer2DUserControl>().enabled = false;
+        
+        StartCoroutine(EndGameWait());
         //end the game when the player enters the trigger
-        SceneManager.LoadScene(2);
+        
 
+    }
+
+    IEnumerator EndGameWait()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(2);
     }
 }
